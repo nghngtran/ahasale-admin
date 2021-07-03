18c3c03c7e051e42e77575ac098b87ad8e2177c0
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2021-07-03 22:17:24 +0700

    update

--- admin.js
+++ admin.js
@@ -33,6 +33,10 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+var server=app.listen(5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
