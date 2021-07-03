6645f06721c137d43e7336138dfe6f8a343a8e65
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2021-07-03 22:03:55 +0700

    update build

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var cors = require('cors')
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
-
+app.use(cors())
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
